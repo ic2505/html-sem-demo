57d07aa9026ab087a4a8edc4db7e1954db55fb47
ikesan <ilkerciliv@gmail.com>
2023-06-08 09:15:23 -0400

    changed colors to pastel

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-let myPet = "Dog";
 
-let myPetsAge = 7;
-
-let petDetails =
-  "My pet is " + myPet + " and it is " + myPetsAge + " years old";
-
-console.log(petDetails);
-
-let myNum = 5;
-let thisNum = ++myNum;
-console.log(thisNum);
-
-// let userInput = prompt("Whats your favorite color?");
-let userInput = null
-
-console.log(userInput);
-
-document.body.style.backgroundColor = userInput;
+const getRandomPastelColor = () => { 
+    return "hsl(" + 360 * Math.random() + ',' +
+               (15 + 80 * Math.random()) + '%,' + 
+               (75 + 20 * Math.random()) + '%)'
+  }
 
 colorButton = document.getElementById("colorButton");
 colorButton.addEventListener("click",  () => {
     console.log("Button clicked");
-    var randomColor = Math.floor(Math.random()*16777215).toString(16);
-    console.log(randomColor);
-    document.body.style.backgroundColor = "#" + randomColor;
+    var randomPastelColor = getRandomPastelColor()
+    console.log(randomPastelColor);
+    document.body.style.backgroundColor = randomPastelColor;
 })
-
